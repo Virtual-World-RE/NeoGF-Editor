3c52438a1d4462435bbe6caf94432927e9796356
Rigodron <djmasifdjpoison@gmail.com>
2023-09-11 13:55:14 +0200

    added HSD classes

---
+++ src/HSD/InverseBindMatrix.java
@@ -0,0 +1,128 @@
+package HSD;
+
+public class InverseBindMatrix {
+    private float rotationXX;
+    private float rotationXY;
+    private float rotationXZ;
+    private float translationX;
+    private float rotationYX;
+    private float rotationYY;
+    private float rotationYZ;
+    private float translationY;
+    private float rotationZX;
+    private float rotationZY;
+    private float rotationZZ;
+    private float translationZ;
+
+    public InverseBindMatrix() {
+        rotationXX = 0.0f;
+        rotationXY = 0.0f;
+        rotationXZ = 0.0f;
+        translationX = 0.0f;
+        rotationYX = 0.0f;
+        rotationYY = 0.0f;
+        rotationYZ = 0.0f;
+        translationY = 0.0f;
+        rotationZX = 0.0f;
+        rotationZY = 0.0f;
+        rotationZZ = 0.0f;
+        translationZ = 0.0f;
+    }
+
+    public float getRotationXX() {
+        return rotationXX;
+    }
+
+    public void setRotationXX(float rotationXX) {
+        this.rotationXX = rotationXX;
+    }
+
+    public float getRotationXY() {
+        return rotationXY;
+    }
+
+    public void setRotationXY(float rotationXY) {
+        this.rotationXY = rotationXY;
+    }
+
+    public float getRotationXZ() {
+        return rotationXZ;
+    }
+
+    public void setRotationXZ(float rotationXZ) {
+        this.rotationXZ = rotationXZ;
+    }
+
+    public float getTranslationX() {
+        return translationX;
+    }
+
+    public void setTranslationX(float translationX) {
+        this.translationX = translationX;
+    }
+
+    public float getRotationYX() {
+        return rotationYX;
+    }
+
+    public void setRotationYX(float rotationYX) {
+        this.rotationYX = rotationYX;
+    }
+
+    public float getRotationYY() {
+        return rotationYY;
+    }
+
+    public void setRotationYY(float rotationYY) {
+        this.rotationYY = rotationYY;
+    }
+
+    public float getRotationYZ() {
+        return rotationYZ;
+    }
+
+    public void setRotationYZ(float rotationYZ) {
+        this.rotationYZ = rotationYZ;
+    }
+
+    public float getTranslationY() {
+        return translationY;
+    }
+
+    public void setTranslationY(float translationY) {
+        this.translationY = translationY;
+    }
+
+    public float getRotationZX() {
+        return rotationZX;
+    }
+
+    public void setRotationZX(float rotationZX) {
+        this.rotationZX = rotationZX;
+    }
+
+    public float getRotationZY() {
+        return rotationZY;
+    }
+
+    public void setRotationZY(float rotationZY) {
+        this.rotationZY = rotationZY;
+    }
+
+    public float getRotationZZ() {
+        return rotationZZ;
+    }
+
+    public void setRotationZZ(float rotationZZ) {
+        this.rotationZZ = rotationZZ;
+    }
+
+    public float getTranslationZ() {
+        return translationZ;
+    }
+
+    public void setTranslationZ(float translationZ) {
+        this.translationZ = translationZ;
+    }
+}
+
---
+++ src/HSD/JObjDesc.java
@@ -0,0 +1,11 @@
+package HSD;
+
+class JObjDesc {
+    int rootJointOffset;         
+    int jointAnimationsOffset;   
+    int materialAnimationsOffset;
+    int shapeAnimationsOffset;   
+    
+    // Constructor, getters et setters
+}
+
